0e92a9b4b9a8676b5b02c74c76ce6d3fc7fc0893
CaiquePires22 <134434597+CaiquePires22@users.noreply.github.com>
2023-07-05 14:22:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,212 @@
+function ampliar(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f2");
+    x3 = document.getElementById("f3");
+    x4 = document.getElementById("f4");
+    x5 = document.getElementById("f5");
+    x6 = document.getElementById("f6");
+    fot = document.getElementById("footera");
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+    letreiro = document.getElementById("H22").innerHTML = "Corrente";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+
+
+    
+}
+
+function ampliar2(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f1");
+    x3 = document.getElementById("f3");
+    x4 = document.getElementById("f4");
+    x5 = document.getElementById("f5");
+    x6 = document.getElementById("f6");
+    fot = document.getElementById("footera");
+    letreiro = document.getElementById("H22").innerHTML = "Anel";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+}
+
+function ampliar3(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f2");
+    x3 = document.getElementById("f1");
+    x4 = document.getElementById("f4");
+    x5 = document.getElementById("f5");
+    x6 = document.getElementById("f6");
+    fot = document.getElementById("footera");
+    letreiro = document.getElementById("H22").innerHTML = "Relogio";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+}
+
+function ampliar4(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f2");
+    x3 = document.getElementById("f3");
+    x4 = document.getElementById("f1");
+    x5 = document.getElementById("f5");
+    x6 = document.getElementById("f6");
+    fot = document.getElementById("footera");
+    letreiro = document.getElementById("H22").innerHTML = "Pingente";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+}
+
+function ampliar5(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f2");
+    x3 = document.getElementById("f3");
+    x4 = document.getElementById("f4");
+    x5 = document.getElementById("f1");
+    x6 = document.getElementById("f6");
+    fot = document.getElementById("footera");
+    letreiro = document.getElementById("H22").innerHTML = "Pulseira";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+}
+
+function ampliar6(id) {
+    var x2;
+    var x3;
+    var x4;
+    var x5;
+    var x6;
+    var fot;
+    var letreiro;
+
+    
+    x2 = document.getElementById("f2");
+    x3 = document.getElementById("f3");
+    x4 = document.getElementById("f4");
+    x5 = document.getElementById("f5");
+    x6 = document.getElementById("f1");
+    fot = document.getElementById("footera");
+    letreiro = document.getElementById("H22").innerHTML = "Brinco";
+    x2.style.display = "none";
+    x3.style.display = "none";
+    x4.style.display = "none";
+    x5.style.display = "none";
+    x6.style.display = "none";
+    fot.style.display = "none";
+
+    id.style.width = "50%";
+    id.style.height = "70%";
+    id.style.position = "absolute";
+    id.style.animationName = "testando";
+    id.style.hover.animationDuration = "0.5s";
+    id.style.hover.animationDelay = "0s";
+    id.style.hover.animationTimingFunction = "linear";
+    id.style.hover.animationDirection = "Normal";
+    id.style.hover.animationFillMode = "Forwards";
+}
